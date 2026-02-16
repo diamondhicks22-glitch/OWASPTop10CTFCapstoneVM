d29103c542ef927cd9d0516c4c3f0798a76f2630
cpsc498admin <diamond.hicks.22@cnu.edu>
2026-02-16 22:50:41 +0000

    Landing page skeleton

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include 'connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>OWASP-Inspired CTF Site</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!--ADD CSS LINK HERE-->
+</head>
+
+<body>
+	<header>
+		<!--ADD NAV HERE-->
+	</header>
+
+	<main>
+	<h1>OWASP-Inspired CTF Site</h1>
+	<p>All links are currently non-functioning while assets are being developed</p>
+	<a>Introduction</a><br>
+	<a>Level1TITLE</a><br>
+	<a>Level2TITLE</a><br>
+	<a>Level3TITLE</a><br>
+	<a>Level4TITLE</a><br>
+	<a>Level5TITLE</a><br>
+	<a>Level6TITLE</a><br>
+	<a>Level7TITLE</a><br>
+	<a>Level8TITLE</a><br>
+	<a>Level9TITLE</a><br>
+	<a>Level10TITLE</a><br>
