506cb439e9606ebd57a9fb05148710bb97c39483
cpsc498admin <diamond.hicks.22@cnu.edu>
2026-02-24 19:49:39 +0000

    CSS started

--- index.php
+++ index.php
@@ -8,38 +8,39 @@ include '.connect.php';
 	<meta charset="UTF-8">
 	<title>OWASP-Inspired CTF Site</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!--ADD CSS LINK HERE-->
+	<link rel="stylesheet" href="sitecss.css">
 </head>
 
 <body>
 	<header>
+		<h1>OWASP-Inspired CTF Site</h1>
+		<p>All links are currently non-functioning while assets are being developed</p>
+
 		<!--ADD NAV HERE-->
 	</header>
 
 	<main>
-	<h1>OWASP-Inspired CTF Site</h1>
-	<p>All links are currently non-functioning while assets are being developed</p>
-	<a href='base-site.php'>Introduction</a>
-	<!-- <?php
-	$query = $connection->prepare("SELECT completed FROM 2025_levels WHERE ID = 0");
+	<div class="levels">
+	<?php
+	$query = $connection->prepare("SELECT completed, title, location  FROM 2025_levels");
 	$query->execute();
 	$result = $query->get_result();
-	if ($result->num_rows > 0) {
-		$row = $result->fetch_assoc();
-		$complete = $row['completed'];
+	while ($row = $result->fetch_assoc()) {
+		if ($row['completed']) {
+			echo "<div class='levelblock completed'>";
+		}
+		else {
+			echo "<div class='levelblock'>";
+		}
+		echo "<h3>";
+		echo "<a href=" . htmlspecialchars($row['location']) . ">" . htmlspecialchars($row['title']) . "</a>";
+		echo "</h3>";
+		echo "</div>";
 	}
-	else{
-		$complete = null;
-	}
-	if ($complete === null) {
-		echo "Level not found";
-	} elseif ($complete) {
-		echo "Level completed";
-	} else {
-		echo "Level incomplete";
-	}
-?> -->
-	<br>
+?>
+	
+	</div>
+	<br><p> Still being developed </p>
 	<a>Level1TITLE</a><br>
 	<a>Level2TITLE</a><br>
 	<a>Level3TITLE</a><br>
@@ -55,3 +56,4 @@ include '.connect.php';
 	<button type="submit">Reset Progress</button>
 	</form>
 </body>
+</html>
