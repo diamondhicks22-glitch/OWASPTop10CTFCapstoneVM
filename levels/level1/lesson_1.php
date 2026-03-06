ab6074ba5a74e3217c7e305bc51883e4cc12b52a
Diamond <diamond.hicks.22@cnu.edu>
2026-03-06 15:42:03 -0500

    Added image folder and fixed lesson_1.php typos

--- levels/level1/lesson_1.php
+++ levels/level1/lesson_1.php
@@ -20,14 +20,14 @@ require_once __DIR__ . '/../../secure_assets/.connect.php';
 	<main>
 	<h2>What?</h2>
 	<strong>Security Logging and Alerting Failures</strong>
-	<p>A vulnerability that exposes important information via an error or within accesible logs.
+	<p>A vulnerability that exposes important information via an error or within accessible logs.
 	While the program may not break from the error, it gives the user and possible attacker information
 	that can aid in exploitation. </p>
 	<br>
 	
 	<h2>Why?</h2>
 	<p>Some errors are explicit to certain operating systems and web applications eg. Blue Screen of
-	Death (BSOD) errors on Windows or rpmdb errors on Linux/Unix enviroments. Others, like the one in this lab, give information
+	Death (BSOD) errors on Windows or rpmdb errors on Linux/Unix environments. Others, like the one in this lab, give information
 	 about user accounts and aid in brute force attacks or others of similar execution. 
 	While a system must handle errors gracefully and logging is essential, confidentiality is a major factor when doing so. 
 	Errors should be logged internally where someone within the managing IT system can see and investigate and not be available to the user.
@@ -36,10 +36,11 @@ require_once __DIR__ . '/../../secure_assets/.connect.php';
 	
 	<h2>How?</h2>
 	<p>In this lab, the example includes a simple case of "echo"-ing the error alongside an error that gives too much information.</p>
-	<img>
+	<img src="/../../image_assets/lesson_1/toomucherror.png">
+	<img src="/../../image_assets/lesson_1/errorecho.png">
 	<p>A form of fix would making the error internal if specifications are needed, in this case error_log, and only displaying to the user
 	the needed information via the die() command.</p>
-	<img>
+	<img src="/../../image_assets/lesson_1/lesson1_fix.png">
 
 	<h2>Further information</h2>
 	<a href="https://owasp.org/Top10/2025/A09_2025-Security_Logging_and_Alerting_Failures/" target="_blank" rel='noopener noreferrer'>OWASP Security Logging and Alerting Failures</a>
